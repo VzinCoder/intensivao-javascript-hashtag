6075d5b65aa17d6f82dc13f562a58e5c55583dc6
VzinCoder <contato.viniciusp.b@gmail.com>
2024-02-03 16:44:24 -0300

    completes the project01

---
+++ projeto01/app.js
@@ -0,0 +1,79 @@
+const btnContainer = document.getElementById('button-container');
+const audio = document.querySelector('audio');
+const btnPlayPause = document.getElementById('play-pause');
+
+const getAudioInfo = () => {
+   const currentChapter = Number(audio.dataset.chapter);
+   const minLimit = Number(audio.dataset.minLimit);
+   const maxLimit = Number(audio.dataset.maxLimit);
+   return { currentChapter, minLimit, maxLimit };
+};
+
+const setChapter = (chapter) => {
+   audio.src = `./books/dom-casmurro/${chapter}.mp3`;
+   audio.dataset.chapter = chapter;
+};
+
+const updateChapter = (delta) => {
+   const { currentChapter, minLimit, maxLimit } = getAudioInfo();
+   const newChapter = currentChapter + delta;
+   const isWithinLimits = newChapter >= minLimit && newChapter <= maxLimit;
+   const updatedChapter = isWithinLimits ? newChapter : delta > 0 ? minLimit : maxLimit;
+   setChapter(updatedChapter);
+   playAndPause();
+};
+
+const previousChapter = () => updateChapter(-1);
+const nextChapter = () => updateChapter(1);
+
+const setCssClass = (cssClass) => {
+   btnPlayPause.children[0].setAttribute('class', cssClass);
+};
+
+const playAndPause = () => {
+   if (audio.paused) {
+      audio.play();
+      setCssClass('bi bi-pause-circle');
+      return;
+   }
+   audio.pause();
+   setCssClass('bi bi-play-circle');
+};
+
+const checkIdFormat = (idString) => {
+   const isHyphenIncluded = idString.includes('-');
+   if (isHyphenIncluded) {
+      const [playString, pauseString] = idString.split('-');
+      idString = `${playString}${pauseString.replace('p', 'P')}`;
+      return idString;
+   }
+   return idString;
+};
+
+const handleClick = ({ target }) => {
+   const element = target.tagName === 'I' ? target.parentNode : target;
+   let elementId = element.id;
+
+   elementId = checkIdFormat(elementId);
+
+   const options = {
+      previous: previousChapter,
+      next: nextChapter,
+      playPause: playAndPause
+   };
+
+   console.log(elementId)
+
+   const executeAction = options[elementId];
+   executeAction();
+};
+
+btnContainer.addEventListener('click', handleClick);
+
+
+
+
+
+
+
+
